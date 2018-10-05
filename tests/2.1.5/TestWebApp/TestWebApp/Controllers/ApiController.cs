a14d8f42028889472fed258d19abd3dd765c9355
Aksel Arzuman <akselarzuman@gmail.com>
2018-10-05 19:31:41 +0300

    new image folder added and test project updated

---
+++ tests/2.1.5/TestWebApp/TestWebApp/Controllers/ApiController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestWebApp.Controllers
+{
+    [Produces("application/json")]
+    [Route("api")]
+    public class ApiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Ok(new {status = "success"});
+        }
+    }
+}
